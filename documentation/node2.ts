cc51581373fc3581849daa677ba16cbfa6229c1b
Joe Shen <33032611+qshenJoe@users.noreply.github.com>
2021-09-01 09:02:46 +0800

    Update node2.ts

--- documentation/node2.ts
+++ documentation/node2.ts
@@ -1,62 +1,166 @@
-// import G6 from '@antv/g6';
-import { registerLayout } from '@antv/g6';
-const partition = (arr: any[], length: number) => {
-  const result = [];
-  for (let i = 0, j = arr.length; i < j; i++) {
-    if (i % length === 0) {
-      result.push([]);
-    }
-    result[result.length - 1].push(arr[i]);
-  }
-  return result;
-};
-export const registerCustomLayout = () =>
-  registerLayout('d13n-layout', {
-    getDefaultCfg: function getDefaultCfg() {
-      return {
-        center: [0, 0],
-        lineSep: 100,
-        nodeSep: 50,
-        nodeSide: 100,
-      };
-    },
-    execute: function execute() {
-      const self = this;
-      const center = self.center || [0, 0];
-      const nodeSep = self.nodeSep || 250;
-      const nodeSize = self.nodeSize || 100;
-      const marginX = self.marginX || 100;
-      const marginY = self.marginY || 100;
-      console.log('Container Width', self.width);
-      const { nodes, edges } = self;
-      // console.log(nodes);
-      // console.log(edges);
-      const rows = partition(nodes, 5);
-      console.log(rows);
-      const beginX = center[0] + marginX;
-      const beginY = center[1] + marginY;
-      const rowPositions = [];
-      rows.forEach((row: any[], index: number) => {
-        // if index is odd, reverse position x
-        if (index % 2 === 0) {
-          rowPositions.push({
-            x: beginX + (nodeSep * index) / 2,
-            y: beginY + (nodeSep * index) / 2,
-          });
-        } else {
-          rowPositions.push({
-            x: beginX + (nodeSep * (index + 1)) / 2,
-            y: beginY + (nodeSep * (index + 1)) / 2,
-          });
-        }
-      });
-      rows.forEach((row: any[], index: number) => {
-        row.forEach((node: any, idx: number) => {
-          node.x = rowPositions[index].x + idx * nodeSep;
-          node.y = rowPositions[index].y + index * nodeSep;
-          console.log('Node.x', node);
+import { registerNode } from '@antv/g6';
+enum PANEL_COLOR {
+  LIGHT_BLUE = '#d5e7f4',
+  DARK_BLUE = '#025ab2',
+  GREY = '#ddd',
+}
+
+export const registerDeviceNode = () =>
+  registerNode(
+    'd13n-node',
+    {
+      drawShape: function drawShape(cfg, group) {
+        const color = '#dfdfdf';
+        const topRectColor = '#1b75c4';
+        const bottomRectColor = PANEL_COLOR.GREY;
+        const r = 2;
+        const shape = group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 0,
+            width: 150,
+            height: 180,
+            stroke: color,
+            radius: r,
+          },
+          name: 'main-box',
+          draggable: false,
         });
-      });
-      console.log(rowPositions);
+
+        group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 0,
+            width: 150,
+            height: 20,
+            fill: topRectColor,
+            stroke: '#fff',
+            radius: [r, r, 0, 0],
+          },
+          name: 'panel-device-name',
+        });
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            y: 5,
+            x: 2,
+            lineHeight: 20,
+            text: cfg.title,
+            fill: '#fff',
+          },
+          name: 'text-device-name',
+        });
+        group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 20,
+            width: 150,
+            height: 20,
+            fill: topRectColor,
+            stroke: '#fff',
+            radius: [r, r, 0, 0],
+          },
+          name: 'text-device-location',
+        });
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            y: 25,
+            x: 2,
+            lineHeight: 20,
+            text: cfg.uposition,
+            fill: '#fff',
+          },
+          name: 'text-device-uposition',
+        });
+        group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 40,
+            width: 150,
+            height: 100,
+          },
+          name: 'panel-device-type-image',
+        });
+        group.addShape('image', {
+          attrs: {
+            x: 0,
+            y: 40,
+            img: 'assets/models/junction-box.jpeg',
+            width: 150 - 2,
+            height: 100 - 2,
+          },
+          name: 'image-device-type',
+        });
+        group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 140,
+            width: 65,
+            height: 20,
+            stroke: color,
+            fill: topRectColor,
+          },
+          name: 'panel-port-left',
+        });
+        group.addShape('rect', {
+          attrs: {
+            x: 85,
+            y: 140,
+            width: 65,
+            height: 20,
+            stroke: color,
+            fill: topRectColor,
+          },
+          name: 'panel-port-right',
+        });
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            y: 145,
+            x: 2,
+            lineHeight: 20,
+            text: cfg.sPort,
+            fill: '#ddd',
+          },
+          name: 'text-port-left',
+        });
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            textAlign: 'right',
+            y: 145,
+            x: 148, // x=200相当于x=0
+            lineHeight: 20,
+            text: cfg.dPort,
+            fill: '#ddd',
+          },
+          name: 'text-port-right',
+        });
+        group.addShape('rect', {
+          attrs: {
+            x: 0,
+            y: 160,
+            width: 150,
+            height: 20,
+            fill: bottomRectColor,
+          },
+          name: 'panel-device-type',
+        });
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            textAlign: 'center',
+            y: 165,
+            x: 77,
+            lineHeight: 20,
+            text: cfg.deviceType,
+            fill: '#000',
+          },
+          name: 'text-device-type',
+        });
+        return shape;
+      },
     },
-  });
+    'single-node'
+  );
