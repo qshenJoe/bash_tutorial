7ac13d2513d36888ed66f9c6f9716d5dc4997667
Joe Shen <33032611+qshenJoe@users.noreply.github.com>
2021-09-01 09:02:30 +0800

    Update component2.ts

--- documentation/component2.ts
+++ documentation/component2.ts
@@ -1,10 +1,11 @@
+import { registerCustomLayout } from './layout/custom.layout';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import G6 from '@antv/g6';
-import { registerCableEdge } from './edges/cable.edge';
-import { registerDeviceNode } from './nodes/device.node';
+import { registerDeviceNode } from './node/device.node';
+import { registerCableEdge } from './edge/cable.edge';
 
 @Component({
-  selector: 'ux-fiber-doc-view',
+  selector: 'app-fiber-doc-view',
   templateUrl: './fiber-doc-view.component.html',
   styleUrls: ['./fiber-doc-view.component.scss'],
 })
@@ -12,12 +13,14 @@ export class FiberDocViewComponent implements OnInit, AfterViewInit {
   constructor() {}
 
   ngAfterViewInit(): void {
-    registerDeviceNode();
+    registerCustomLayout();
     registerCableEdge();
-    // registerLayout();
-    // registerNode();
+    registerDeviceNode();
     const container = document.getElementById('container');
-    const width = container.scrollWidth;
+    const width = container.scrollWidth || 1200;
+    const margin = 100;
+    const nodesWidth = width - 2 * margin;
+    
     const height = container.scrollHeight;
     const data = {
       // 点集
@@ -57,6 +60,30 @@ export class FiberDocViewComponent implements OnInit, AfterViewInit {
         },
         {
           id: 'node4',
+          title: 'Junction Box 4',
+          uposition: 'Rack test U 5 Slot 14',
+          sPort: 'PORT - L',
+          dPort: 'PORT - R',
+          anchorPoints: [
+            [0, 250 / 300],
+            [1, 250 / 300],
+          ],
+          deviceType: 'PatchPanel#3',
+        },
+        {
+          id: 'node5',
+          title: 'Junction Box 4',
+          uposition: 'Rack test U 5 Slot 14',
+          sPort: 'PORT - L',
+          dPort: 'PORT - R',
+          anchorPoints: [
+            [0, 250 / 300],
+            [1, 250 / 300],
+          ],
+          deviceType: 'PatchPanel#3',
+        },
+        {
+          id: 'node6',
           title: 'Junction Box 4',
           uposition: 'Rack test U 5 Slot 14',
           sPort: 'PORT - L',
@@ -118,7 +145,8 @@ export class FiberDocViewComponent implements OnInit, AfterViewInit {
           source: 'node3',
           target: 'node4',
           sourceAnchor: 1,
-          targetAnchor: 0,
+          targetAnchor: 1,
+          direction: 'RDL',
           style: {
             lineWidth: 10,
             startArrow: {
@@ -138,110 +166,18 @@ export class FiberDocViewComponent implements OnInit, AfterViewInit {
       ],
     };
 
-    const fiberDevices = {
-      nodes: [
-        {
-          id: '0',
-          label: 'd1',
-          cluster: 'row_1',
-        },
-        {
-          id: '1',
-          label: 'd2',
-          cluster: 'row_1',
-        },
-        {
-          id: '2',
-          label: 'd3',
-          cluster: 'row_1',
-        },
-        {
-          id: '3',
-          label: 'd4',
-          cluster: 'row_1',
-        },
-        {
-          id: '4',
-          label: 'd5',
-          cluster: 'row_1',
-        },
-        {
-          id: '5',
-          label: 'd6',
-          cluster: 'row_2',
-        },
-        {
-          id: '6',
-          label: 'd7',
-          cluster: 'row_2',
-        },
-        {
-          id: '7',
-          label: 'd8',
-          cluster: 'row_2',
-        },
-        {
-          id: '8',
-          label: 'd9',
-          cluster: 'row_2',
-        },
-        {
-          id: '9',
-          label: 'd10',
-          cluster: 'row_2',
-        },
-      ],
-      edges: [
-        {
-          source: '0',
-          target: '1',
-        },
-        {
-          source: '1',
-          target: '2',
-        },
-        {
-          source: '2',
-          target: '3',
-        },
-        {
-          source: '3',
-          target: '4',
-        },
-        {
-          source: '4',
-          target: '5',
-        },
-        {
-          source: '5',
-          target: '6',
-        },
-        {
-          source: '6',
-          target: '7',
-        },
-        {
-          source: '7',
-          target: '8',
-        },
-        {
-          source: '8',
-          target: '9',
-        },
-      ],
-    };
-
     const graph = new G6.Graph({
       container: 'container', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
       // width: 800, // Number，必须，图的宽度
       // height: 500, // Number，必须，图的高度
       width,
       height,
-      // layout: {
-      //   type: 'd13n-layout',
-      //   width,
-      //   height,
-      // },
+      layout: {
+        type: 'd13n-layout',
+        width,
+        height,
+        center: [0, 0],
+      },
       modes: {
         default: ['drag-canvas', 'zoom-canvas'],
       },
@@ -254,6 +190,7 @@ export class FiberDocViewComponent implements OnInit, AfterViewInit {
     });
     graph.data(data);
     graph.render();
+    graph.zoomTo(0.8);
   }
 
   ngOnInit() {}
