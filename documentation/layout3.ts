f433ed97ae2aafce8c7fda703bd95a2bbe2f9a55
Joe Shen <33032611+qshenJoe@users.noreply.github.com>
2021-09-01 23:17:11 +0800

    Update layout3.ts

--- documentation/layout3.ts
+++ documentation/layout3.ts
@@ -14,16 +14,14 @@ export const registerCustomLayout = () =>
     execute: function execute() {
       const self = this;
       const center = self.center || [0, 0];
-      const nodeSep = self.nodeSep || 300;
+      const nodeSep = self.nodeSep || 400;
       const nodeSize = self.nodeSize || 100;
       const marginX = self.marginX || 100;
       const marginY = self.marginY || 100;
-      console.log('Container Width', self.width);
       const { nodes, edges } = self;
       // console.log(nodes);
       // console.log(edges);
       const rows = partition(nodes, 5);
-      console.log(rows);
       const beginX = center[0] + marginX;
       const beginY = center[1] + marginY;
       const rowPositions = [];
@@ -43,10 +41,7 @@ export const registerCustomLayout = () =>
             node.x = rowPositions[index].x + (4 - idx) * nodeSep;
             node.y = rowPositions[index].y; // + index * nodeSep;
           }
-          console.log('Node.x', node);
         });
       });
-      console.log('', rows);
-      console.log(rowPositions);
     },
   });
