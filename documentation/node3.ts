95929b7941e65e2f88edb2d444dc4858437d1dc0
Joe Shen <33032611+qshenJoe@users.noreply.github.com>
2021-09-01 19:08:40 +0800

    Update and rename node2.ts to node3.ts

--- documentation/node3.ts
+++ documentation/node3.ts
@@ -1,4 +1,8 @@
 import { registerNode } from '@antv/g6';
+import { findSVG } from '@ngux/shared/path-view/utils';
+import { isEmpty } from '@opi/util';
+import { mix } from '@antv/util';
+
 enum PANEL_COLOR {
   LIGHT_BLUE = '#d5e7f4',
   DARK_BLUE = '#025ab2',
@@ -14,6 +18,8 @@ export const registerDeviceNode = () =>
         const topRectColor = '#1b75c4';
         const bottomRectColor = PANEL_COLOR.GREY;
         const r = 2;
+        const typeId = '822c1b1a-ced5-11e4-b3c2-000c294c98b1'; // cfg.modelId
+        // const style = this.getShapeStyle(cfg);
         const shape = group.addShape('rect', {
           attrs: {
             x: 0,
@@ -45,7 +51,7 @@ export const registerDeviceNode = () =>
             y: 5,
             x: 2,
             lineHeight: 20,
-            text: cfg.title,
+            text: cfg.location ?? '',
             fill: '#fff',
           },
           name: 'text-device-name',
@@ -68,7 +74,7 @@ export const registerDeviceNode = () =>
             y: 25,
             x: 2,
             lineHeight: 20,
-            text: cfg.uposition,
+            text: cfg.container ?? '',
             fill: '#fff',
           },
           name: 'text-device-uposition',
@@ -82,61 +88,64 @@ export const registerDeviceNode = () =>
           },
           name: 'panel-device-type-image',
         });
-        group.addShape('image', {
+        group.addShape('dom', {
           attrs: {
             x: 0,
             y: 40,
-            img: 'assets/models/junction-box.jpeg',
+            html: findSVG(`ux-${typeId}`),
             width: 150 - 2,
             height: 100 - 2,
           },
-          name: 'image-device-type',
         });
-        group.addShape('rect', {
-          attrs: {
-            x: 0,
-            y: 140,
-            width: 65,
-            height: 20,
-            stroke: color,
-            fill: topRectColor,
-          },
-          name: 'panel-port-left',
-        });
-        group.addShape('rect', {
-          attrs: {
-            x: 85,
-            y: 140,
-            width: 65,
-            height: 20,
-            stroke: color,
-            fill: topRectColor,
-          },
-          name: 'panel-port-right',
-        });
-        group.addShape('text', {
-          attrs: {
-            textBaseline: 'top',
-            y: 145,
-            x: 2,
-            lineHeight: 20,
-            text: cfg.sPort,
-            fill: '#ddd',
-          },
-          name: 'text-port-left',
-        });
-        group.addShape('text', {
-          attrs: {
-            textBaseline: 'top',
-            textAlign: 'right',
-            y: 145,
-            x: 148, // x=200相当于x=0
-            lineHeight: 20,
-            text: cfg.dPort,
-            fill: '#ddd',
-          },
-          name: 'text-port-right',
-        });
+        if (cfg.sPort) {
+          group.addShape('rect', {
+            attrs: {
+              x: 0,
+              y: 140,
+              width: 65,
+              height: 20,
+              stroke: color,
+              fill: topRectColor,
+            },
+            name: 'panel-port-left',
+          });
+          group.addShape('text', {
+            attrs: {
+              textBaseline: 'top',
+              y: 145,
+              x: 2,
+              lineHeight: 20,
+              text: cfg.sPort,
+              fill: '#ddd',
+            },
+            name: 'text-port-left',
+          });
+        }
+        if (cfg.dPort) {
+          group.addShape('rect', {
+            attrs: {
+              x: 85,
+              y: 140,
+              width: 65,
+              height: 20,
+              stroke: color,
+              fill: topRectColor,
+            },
+            name: 'panel-port-right',
+          });
+          group.addShape('text', {
+            attrs: {
+              textBaseline: 'top',
+              textAlign: 'right',
+              y: 145,
+              x: 148, // x=200相当于x=0
+              lineHeight: 20,
+              text: cfg.dPort,
+              fill: '#ddd',
+            },
+            name: 'text-port-right',
+          });
+        }
         group.addShape('rect', {
           attrs: {
             x: 0,
@@ -159,7 +168,24 @@ export const registerDeviceNode = () =>
           },
           name: 'text-device-type',
         });
+        // if (style.img || style.html) {
+        //   group.addShape(style.html ? 'dom' : 'image', {
+        //     attrs: { ...style, style: 'pointer-events: none;' }, // { pointerEvents: 'none' }
+        //   });
+        // }
         return shape;
+      },
+      getShapeStyle(cfg) {
+        const typeId = cfg.modelId; // '822c1b1a-ced5-11e4-b3c2-000c294c98b1';
+        const img = null;
+        const style = mix({}, cfg.style, {
+          x: 0, // 节点的位置在上层确定，所以这里仅使用相对位置即可
+          y: 40,
+          width: 150 - 2,
+          height: 100 - 2,
+          ...(typeId ? { html: findSVG(`ux-${typeId}`) } : { img }),
+        });
+        return style;
       },
     },
     'single-node'
