d995000c34327c53882de0002907e2219b5cf415
Joe Shen <33032611+qshenJoe@users.noreply.github.com>
2021-09-05 18:59:51 +0800

    Update component3.ts

--- documentation/component3.ts
+++ documentation/component3.ts
@@ -1,208 +1,90 @@
-import { registerCustomLayout } from './layout/custom.layout';
-import {
-  AfterViewInit,
-  Component,
-  OnInit,
-  OnDestroy,
-  Input,
-  ElementRef,
-} from '@angular/core';
-import G6 from '@antv/g6';
-import { registerDeviceNode } from './nodes/device.node';
-import { registerCableEdge } from './edges/cable.edge';
+import { MOCK_DATA } from './data/data';
 import { transformData } from './utils';
-import { combineLatest, merge, ReplaySubject, Subject } from 'rxjs';
-import { IconService } from '@ngux/component/icon';
-import { debounceTime, takeUntil } from 'rxjs/operators';
-import { FiberDocViewService } from './fiber-doc-view.service';
-import { ActivatedRoute } from '@angular/router';
-import { DefaultBTDetailToolbar } from '@ngux/shared/table';
-import { BasicToolbarMenuTypeEnum } from '@ngux/shared/toolbar/basic-toolbar';
-import { TRANSLATE } from '@ngux/util';
-import { MOCK_DATA } from '@ngux/shared/fiber-doc-view/data';
-import { CustomBaseEvent, nextTick } from '@opi/util';
-import { GlobalService } from '@opi/abc';
+import { registerCustomLayout } from './layout/custom.layout';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import G6, { IG6GraphEvent } from '@antv/g6';
+import { registerDeviceNode } from './node/device.node';
+import { registerCableEdge } from './edge/cable.edge';
 
 @Component({
-  selector: 'ux-fiber-doc-view',
+  selector: 'app-fiber-doc-view',
   templateUrl: './fiber-doc-view.component.html',
   styleUrls: ['./fiber-doc-view.component.scss'],
 })
-export class FiberDocViewComponent implements OnInit, AfterViewInit, OnDestroy {
-  destroyed$$ = new Subject<void>();
-  private data$$ = new ReplaySubject<any[]>(1);
-  graph: any;
-  portOptionLabel: string;
-  portOptionType: 'simplex' | 'duplex' | 'mtp';
-  duplexPositionData: any[];
-  mtpStrandData: any[];
-  selectedStrand: number = 1;
-
-  toolbar = new DefaultBTDetailToolbar({
-    hostKey: 'path-view.info',
-    title: TRANSLATE('common.circuit_trace') as any,
-    // accessKey: AclModuleKeys.PATH_VIEW;
-    menus: {
-      [BasicToolbarMenuTypeEnum.EXPORT]: {
-        keys: ['xlsx'], //  'pdf'
-      },
-      [BasicToolbarMenuTypeEnum.REFRESH_LIST]: {},
-      [BasicToolbarMenuTypeEnum.NEW_FAVORITE]: {},
-    },
-  });
-
-  @Input() set data(value: any) {
-    if (value == null) {
-      return;
-    }
-    this.data$$.next(value);
-  }
-
-  get nativeElement() {
-    return this.elementRef.nativeElement;
-  }
-
-  constructor(
-    public service: FiberDocViewService,
-    private router: ActivatedRoute,
-    private iconService: IconService,
-    private elementRef: ElementRef,
-    private globalService: GlobalService
-  ) {}
+export class FiberDocViewComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
   ngAfterViewInit(): void {
-    this.init();
-  }
+    registerCustomLayout();
+    registerCableEdge();
+    registerDeviceNode();
+    const container = document.getElementById('container');
+    const width = container.scrollWidth || 1200;
+    const margin = 100;
+    const nodesWidth = width - 2 * margin;
 
-  ngOnInit() {
-    this.service.dataSource$$.pipe(this.autoDestroyed()).subscribe(result => {
-      this.setData(result);
+    const height = container.scrollHeight;
+
+    const graph = new G6.Graph({
+      container: 'container', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
+      // width: 800, // Number，必须，图的宽度
+      // height: 500, // Number，必须，图的高度
+      width,
+      height,
+      fitView: true,
+      renderer: 'svg',
+      layout: {
+        type: 'd13n-layout',
+        width,
+        height,
+        cableBaseWidth: 180,
+        marginX: 200,
+        center: [0, 0],
+      },
+      modes: {
+        default: ['drag-canvas', 'zoom-canvas'],
+      },
+      defaultNode: {
+        type: 'd13n-node',
+      },
+      defaultEdge: {
+        type: 'cable-edge',
+      },
     });
-    this.router.data.pipe(this.autoDestroyed()).subscribe(result => {});
-    this.service.portIdxSource$$
-      .pipe(this.autoDestroyed())
-      .subscribe(result => {
-        this.loadButtons(result);
-        console.log(result);
-      });
+    graph.data(transformData(width, MOCK_DATA));
+    graph.render();
+    graph.zoomTo(0.8);
 
-    combineLatest([
-      this.globalService.resize$,
-      this.globalService.mainSideNavStatus$$,
-      this.globalService.deviceCentralLeftStatus$$,
-      this.globalService.deviceCentralTileStatus$$,
-    ])
-      .pipe(debounceTime(120), this.autoDestroyed())
-      .subscribe(() => {
-        nextTick(() => {
-          if (this.graph && this.nativeElement) {
-            this.graph.changeSize(
-              this.nativeElement.offsetWidth,
-              this.nativeElement.offsetHeight
-            );
-          }
-        }, 500);
-      });
+    graph.on('edge:click', (ev) => {
+      const edge = ev.item;
+      this.runWhenNameIs(ev, 'cable-path-main', () =>
+        graph.setItemState(edge, 'selected', !edge.hasState('selected'))
+      );
+    });
+
+    graph.on('edge:mouseenter', (ev) => {
+      const edge = ev.item;
+      this.runWhenNameIs(ev, 'cable-path-main', () =>
+        graph.setItemState(edge, 'active', true)
+      );
+    });
+    graph.on('edge:mouseleave', (ev) => {
+      const edge = ev.item;
+      // graph.setItemState(edge, 'active', false);
+      graph.get('canvas').setCursor('default');
+    });
+    graph.on('node:click', (ev) => {
+      const node = ev.item;
+      console.log(node);
+    });
   }
 
-  setData(data: any) {
-    this.data = transformData(data);
-  }
+  ngOnInit() {}
 
-  init() {
-    combineLatest([this.data$$, this.iconService.loadingCompleted$$])
-      .pipe(debounceTime(100), this.autoDestroyed())
-      .subscribe(([data]) => {
-        if (this.graph != null) {
-          // && preType !== type
-          this.graph.destroy();
-          this.graph = null;
-        }
-        if (this.graph == null) {
-          const width = this.nativeElement.offsetWidth;
-          const height = this.nativeElement.offsetHeight - 60;
-          const container = document.getElementById('container');
-          // const width = container.scrollWidth || 1200;
-          // const height = container.scrollHeight;
-          this.graph = new G6.Graph({
-            container: this.nativeElement, //  'container', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-            width,
-            height,
-            renderer: 'svg',
-            fitView: true,
-            layout: {
-              type: 'd13n-layout',
-              width,
-              height,
-              center: [0, 0],
-            },
-            modes: {
-              default: ['drag-canvas', 'zoom-canvas'],
-            },
-            defaultNode: {
-              type: 'd13n-node',
-            },
-            defaultEdge: {
-              type: 'cable-edge',
-            },
-          });
-          // this.graph.data(transformData(MOCK_DATA));
-          this.graph.data(data);
-          this.graph.render();
-          this.graph.zoomTo(0.8);
-        } else {
-          this.graph.changeData(data);
-        }
-      });
-  }
-
-  loadButtons(config: {
-    type: string;
-    list?: { key: string; value: number }[];
-  }) {
-    const { type, list } = config;
-    if (type === 'simple') {
-      // hide buttons
-      this.portOptionLabel = null;
-      this.portOptionType = 'simplex';
-    } else if (type === 'duplex') {
-      this.duplexPositionData = list.map((item: any) => {
-        const { value, key } = item;
-        return {
-          label: value,
-          id: key,
-          name: 'duplex-position-radio',
-        };
-      });
-      this.portOptionLabel = 'Duplex Position';
-      this.portOptionType = 'duplex';
-    } else if (type === 'mtp') {
-      this.mtpStrandData = list;
-      this.portOptionLabel = 'Strand';
-      this.portOptionType = 'mtp';
+  runWhenNameIs(ev: IG6GraphEvent, name: string, fn: Function) {
+    const t = ev.target.get('name');
+    if (t === name) {
+      fn();
     }
-  }
-
-  handlerChangePortIdxEvent(event: CustomBaseEvent, type: 'duplex' | 'mtp') {
-    const { value } = event;
-    if (type === 'duplex') {
-      this.service.refresh({ idx: value });
-    } else if (type === 'mtp') {
-      this.selectedStrand = value;
-      this.service.refresh({ idx: value });
-    }
-  }
-
-  autoDestroyed<T>(...obs: Subject<any>[]) {
-    return obs && obs.length > 0
-      ? takeUntil<T>(merge(...obs, this.destroyed$$))
-      : takeUntil<T>(this.destroyed$$);
-  }
-
-  ngOnDestroy() {
-    this.destroyed$$.next();
-    this.destroyed$$.unsubscribe();
-    this.service.dataSource$$.next(null);
-    this.service.portIdxSource$$.next(null);
   }
 }
